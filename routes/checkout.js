85e494ea2a6d06dddf3330d78aeace68f63c5e09
BiigMoney <64580434+BiigMoney@users.noreply.github.com>
2021-11-12 13:41:44 -0800

    actually done order.js + password

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 
-router.get('/', function(req, res, next) {
-    res.setHeader('Content-Type', 'text/html');
-    res.write("<title>Grocery CheckOut Line</title>");
+router.get("/", function (req, res, next) {
+  res.setHeader("Content-Type", "text/html")
+  res.write("<title>Grocery CheckOut Line</title>")
 
-    res.write("<h1>Enter your customer id to complete the transaction:</h1>");
+  res.write("<h1>Enter your customer id to complete the transaction:</h1>")
 
-    res.write('<form method="get" action="order">');
-    res.write('<input type="text" name="customerId" size="50">');
-    res.write('<input type="submit" value="Submit"><input type="reset" value="Reset">');
-    res.write('</form>');
+  res.write('<form method="get" action="order">')
+  res.write('<input type="text" name="customerId" placeHolder="ID" size="50">')
+  res.write('<input type="text" name="password" placeHolder="Password" size="50">')
+  res.write('<input type="submit" value="Submit"><input type="reset" value="Reset">')
+  res.write("</form>")
 
-    res.end();
-});
+  res.end()
+})
 
-module.exports = router;
+module.exports = router
